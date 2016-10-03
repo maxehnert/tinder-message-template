015a4e115c10cb6afec5fcb8c91913e664f2b8fe
Max Ehnert <max.ehnert@gmail.com>
2016-10-03 17:19:48 -0500

    Removed the router temporarily.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute, IndexRedirect } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute, IndexRedirect, hashHistory } from 'react-router'
 
 // import App from './components/App';
 // import Home from './components/Home';
@@ -9,11 +9,18 @@ import { App, Home } from './components/index'
 import 'bootstrap/dist/css/bootstrap.css'
 import './css/index.css';
 
+/**
+ * Don't technically need the router yet so I'm leavinig it disabled because it's
+ * making deployment to gh-pages more difficult.
+*/
+// render((
+//   <Router history={hashHistory}>
+//     <Route path="/" component={App}>
+//       <IndexRoute component={Home} />
+//     </Route>
+//   </Router>
+// ),document.getElementById('root'));
 
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-    </Route>
-  </Router>
-),document.getElementById('root'));
+  <Home />
+), document.getElementById('root'))
